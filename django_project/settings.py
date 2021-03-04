05e2d90e07a1a9e5101e0a2fc8cccec4ecd79d74
richanderson <richcanderson98@gmail.com>
2021-03-04 15:25:09 -0600

    django heroku

--- django_project/settings.py
+++ django_project/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,3 +123,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+import django_heroku
+django_heroku.settings(locals())
