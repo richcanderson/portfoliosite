6534b3ca799a8016001714d523c36a3684306c68
richanderson <richcanderson98@gmail.com>
2021-03-06 08:34:16 -0600

    bug fixes

--- django_project/settings.py
+++ django_project/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+import secrets
+from os import getenv
+from secrets import *
 
 
 from pathlib import Path
@@ -22,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+acmsst5s69k-%502hv2&^g_u7)(peie_!043723a2x8q+qica'
+with open('D:\python\django_project\django_project\secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
